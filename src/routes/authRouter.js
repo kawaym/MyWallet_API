fbd0fdcbc8b9bdcf18a30fd7db6a087835f2ee93
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-02-06 13:34:25 -0300

    feat: adds basic server structure

---
+++ src/routes/authRouter.js
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { signIn } from '../controllers/authController.js';
+
+const authRouter = Router();
+authRouter.post('/', signIn);
+export default authRouter;
