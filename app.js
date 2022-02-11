60711dc1e94b417d5462ea16b4cf8117e0265dcb
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-02-11 18:05:00 -0300

    chore: prepares heroku deploy

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port" + process.env.PORT);
 });
