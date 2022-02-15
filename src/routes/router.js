3eb8065c421457386486d358bc2ced2990513c99
Liaess <yann.melo@gmail.com>
2022-02-15 14:43:14 -0300

    Preparando revisão de código

--- src/routes/router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import transactionRouter from './transactionRouter.js';
-import userRouter from './userRouter.js';
-
-const router = Router();
-router.use(userRouter);
-router.use(transactionRouter);
-export default router;
